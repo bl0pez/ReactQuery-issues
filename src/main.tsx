cacc01d71fd887c4e913ffa9271bead2b61b706c
bl0pez <blopez4434@gmail.com>
2023-02-13 08:28:45 -0300

    configurando react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 //! Remover enable css source maps
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
 
+
+// Crear un nuevo cliente de react-query
+const client = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
+    <QueryClientProvider client={client}>
+      {/* Herramientas de desarrollo de react-query */}
+      <ReactQueryDevtools />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
